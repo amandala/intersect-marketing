706a96dce185afbca52cdc508138075b09010d15
Amanda Haynes <dev@portad.ca>
2019-12-11 19:40:15 -0700

    Remove unused components

--- src/screens/BridgeStage/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const BridgeStage = () => {
-  return <p>BridgeStage</p>;
-};
-
-export default BridgeStage;
--- src/screens/GalleyStage/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const GalleyStage = () => {
-  return <p>GalleyStage</p>;
-};
-
-export default GalleyStage;
