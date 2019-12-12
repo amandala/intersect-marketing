5a9f551e4e181df8b2c361c9989686fd9d432aff
Amanda Haynes <dev@portad.ca>
2019-12-12 09:48:51 -0700

    remove unused imports

--- src/screens/index.jsx
+++ src/screens/index.jsx
@@ -1,7 +1,4 @@
 export { default as About } from "./About";
-export { default as BridgeStage } from "./BridgeStage";
-export { default as CoreStage } from "./CoreStage";
 export { default as FAQ } from "./FAQ";
-export { default as GalleyStage } from "./GalleyStage";
 export { default as Home } from "./Home";
 export { default as Tickets } from "./Tickets";
