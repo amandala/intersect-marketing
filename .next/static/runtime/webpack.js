974ace677ba5285fc750fdc523f592aa262add87
Amanda <amanda.haynes@versett.com>
2020-01-02 14:17:07 -0700

    add webpack

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5ee873eccf201318fe5b";
+/******/ 	var hotCurrentHash = "9fc205598f6ea9123656";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
